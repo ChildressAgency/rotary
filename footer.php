6c5e370972001b7fc63af2a6601f42f760562d50
halfg33k <johnsonam17@gmail.com>
2018-09-12 15:00:26 -0400

    added content to footer

--- footer.php
+++ footer.php
@@ -1,4 +1,39 @@
 	<footer>
+        <div class="section container">
+            <div class="row">
+                <div class="col-4">
+                    <h3 class="footer__heading">Meeting Location</h3>
+                    <p>Wednesday - 11:45 AM</p>
+                    <p>Riverside Center<br/>95 Riverside Parkway<br/>Fredericksburg, VA 22406</p>
+                </div>
+                <div class="col-4 side-borders">
+                    <h3 class="footer__heading">Browse Our Site</h3>
+                    <div class="row">
+                        <ul class="col-6 footer__menu">
+                            <li><a href="#"><strong>About Our Club</strong></a></li>
+                            <li><a href="#"><strong>District Links</strong></a></li>
+                            <li><a href="#"><strong>About Rotary</strong></a></li>
+                        </ul>
+                        <ul class="col-6 footer__menu">
+                            <li><a href="#"><strong>Get Involved</strong></a></li>
+                            <li><a href="#"><strong>News and Stories</strong></a></li>
+                            <li><a href="#"><strong>Calendar</strong></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <h3 class="footer__heading">Mail</h3>
+                    <p>Rotary Club of Stafford<br/>P.O. Box 5575<br/>Fredericksburg, VA 22403</p>
+                </div>
+            </div>
+            <div class="vert-spacer"></div>
+            <div class="copyright">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/wheel.png" class="float-left">
+                <p><strong>Thanks to our <a class="copyright__link" href="#">Sponsors</a><br/>Website Design by <a class="copyright__link" href="">The Childress Agency</a></strong></p>
+            </div>
+        </div>
+
+        
     </footer>
 	
 	<?php wp_footer(); ?>
