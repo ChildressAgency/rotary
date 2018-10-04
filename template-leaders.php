144a8f81be8b17443119be811f966de85734d240
halfg33k <johnsonam17@gmail.com>
2018-10-04 14:18:23 -0400

    refactored from 'people' to 'leaders'

--- template-leaders.php
+++ template-leaders.php
@@ -7,23 +7,23 @@
 get_header(); ?>
 
     <main id="main">
-        <?php if( have_rows( 'people' ) ): ?>
+        <?php if( have_rows( 'leaders' ) ): ?>
             <div class="container">
                 <div class="section">
-                    <div class="people">
-                        <div class="people__row row">
-                            <?php $i = 0; while( have_rows( 'people' ) ): the_row(); ?>
+                    <div class="leaders">
+                        <div class="leaders__row row">
+                            <?php $i = 0; while( have_rows( 'leaders' ) ): the_row(); ?>
                                 <?php if( $i != 0 && $i % 2 == 0 ): ?>
                                     </div>
-                                    <div class="people__row row">
+                                    <div class="leaders__row row">
                                 <?php endif; ?>
 
                                 <div class="col-12 col-sm-6">
-                                    <img class="people__img" src="<?php the_sub_field( 'image' ); ?>">
-                                    <div class="people__info">
-                                        <h4 class="people__name"><?php the_sub_field( 'first_name' ); ?> <?php the_sub_field( 'last_name' ); ?></h4>
-                                        <p class="people__title"><?php the_sub_field( 'title' ); ?></p>
-                                        <a href="mailto:<?php the_sub_field( 'email_address' ); ?>" class="people__email"><i class="fas fa-envelope"></i> Email <?php the_sub_field( 'first_name' ); ?></a>
+                                    <img class="leaders__img" src="<?php the_sub_field( 'image' ); ?>">
+                                    <div class="leaders__info">
+                                        <h4 class="leaders__name"><?php the_sub_field( 'first_name' ); ?> <?php the_sub_field( 'last_name' ); ?></h4>
+                                        <p class="leaders__title"><?php the_sub_field( 'title' ); ?></p>
+                                        <a href="mailto:<?php the_sub_field( 'email_address' ); ?>" class="leaders__email"><i class="fas fa-envelope"></i> Email <?php the_sub_field( 'first_name' ); ?></a>
                                     </div>
                                 </div>
                             <?php $i++; endwhile; ?>
