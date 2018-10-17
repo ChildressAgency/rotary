cae17feb69b8d89b4b6b7cbb3754567095ccb960
halfg33k <johnsonam17@gmail.com>
2018-10-17 11:26:55 -0400

    added footer text to options page

--- footer.php
+++ footer.php
@@ -2,9 +2,10 @@
         <div class="section container">
             <div class="row">
                 <div class="col-12 col-md-4">
-                    <h3 class="footer__heading">Meeting Location</h3>
+                    <!-- <h3 class="footer__heading">Meeting Location</h3>
                     <p>Wednesday - 11:45 AM</p>
-                    <p>Riverside Center<br/>95 Riverside Parkway<br/>Fredericksburg, VA 22406</p>
+                    <p>Riverside Center<br/>95 Riverside Parkway<br/>Fredericksburg, VA 22406</p> -->
+                    <?php the_field( 'footer_section_left', 'option' ); ?>
                 </div>
                 <div class="col-12 col-md-4 side-borders">
                     <h3 class="footer__heading">Browse Our Site</h3>
@@ -22,14 +23,16 @@
                     </div>
                 </div>
                 <div class="col-12 col-md-4">
-                    <h3 class="footer__heading">Mail</h3>
-                    <p>Rotary Club of Stafford<br/>P.O. Box 5575<br/>Fredericksburg, VA 22403</p>
+                    <!-- <h3 class="footer__heading">Mail</h3>
+                    <p>Rotary Club of Stafford<br/>P.O. Box 5575<br/>Fredericksburg, VA 22403</p> -->
+                    <?php the_field( 'footer_section_right', 'option' ); ?>
                 </div>
             </div>
             <div class="vert-spacer"></div>
             <div class="copyright">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/wheel.png" class="float-left">
-                <p><strong>Thanks to our <a class="copyright__link" href="#">Sponsors</a><br/>Website Design by <a class="copyright__link" href="">The Childress Agency</a></strong></p>
+                <!-- <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/wheel.png" class="float-left"> -->
+                <!-- <p><strong>Thanks to our <a class="copyright__link" href="#">Sponsors</a><br/>Website Design by <a class="copyright__link" href="">The Childress Agency</a></strong></p> -->
+                <?php the_field( 'copyright_text', 'option' ); ?>
             </div>
         </div>
 
